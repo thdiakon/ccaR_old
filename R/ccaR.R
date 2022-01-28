0bc43f4dfdf352912460cd60e76152a93a3d74d5
thdiakon <thdiakon@users.noreply.github.com>
2022-01-28 07:54:21 +0200

    Add files via upload

--- R/ccaR.R
+++ R/ccaR.R
@@ -17,6 +17,7 @@ cca <- function(cm){
     studies<-nrow(cm)
     reviews<-ncol(cm)
 
+    overlap_counts <- c()
     N <-c()
     r<-c()
     c<-c()
@@ -32,6 +33,8 @@ cca <- function(cm){
 
         reviews[i]<-paste(colnames(cm2[1]), "vs." ,colnames(cm2[2]))
 
+        overlap_counts[i] <- nrow(cm2[as.logical(rowSums(cm2) == 2), ])
+
         N[i]<- sum(cm2)
 
         r[i]<-nrow(cm2[as.logical(rowSums(cm2 != 0)), ])
@@ -40,18 +43,20 @@ cca <- function(cm){
 
         CCA_Proportion[i] <- (N[i]-r[i])/((r[i]*c[i])-r[i])
 
-        CCA_Percentage[i] <- CCA_Proportion[i]*100
+        CCA_Percentage[i] <- round(CCA_Proportion[i]*100, digits = 1)
     }
 
     overall<-nrow(j)+1
     reviews[overall]<-"Overall"
+    overlap_counts[overall] <- " "
     N[overall]<-sum(cm)
     r[overall]<-nrow(cm)
     c[overall]<-ncol(cm)
     CCA_Proportion[overall] <- (N[overall]-r[overall])/((r[overall]*c[overall])-r[overall])
-    CCA_Percentage[overall] <- CCA_Proportion[overall]*100
+    CCA_Percentage[overall] <- round(CCA_Proportion[overall]*100, digits = 1)
 
-    res<- data.frame(reviews,N,r,c,CCA_Proportion,CCA_Percentage,stringsAsFactors=FALSE)
+    res<- data.frame(reviews,overlap_counts,
+    N,r,c,CCA_Proportion,CCA_Percentage,stringsAsFactors=FALSE)
 
 #    return(res)
 
